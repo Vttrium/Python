548f58f1d6768e02c0c456f2d58df68bb21e1a70
Vttrium <phsauthier@yahoo.com>
2024-12-23 14:53:26 -0300

    tickets auto

--- Macro/AutoGui/macro.py
+++ Macro/AutoGui/macro.py
@@ -2,158 +2,98 @@ import pyautogui as pa
 import time
 
 def play_macro():
-    time.sleep(2.89)
-    pa.click(82, 143)  # Mouse click at (82, 143)
-    time.sleep(1.01)
-    pa.click(95, 204)  # Mouse click at (95, 204)
-    time.sleep(2.29)
-    pa.click(1159, 204)  # Mouse click at (1159, 204)
-    time.sleep(1.37)
-    pa.click(1163, 393)  # Mouse click at (1163, 393)
-    time.sleep(2.85)
-    pa.click(1000, 409)  # Mouse click at (1000, 409)
-    time.sleep(1.83)
-    pa.click(476, 259)  # Mouse click at (476, 259)
-    time.sleep(1.57)
-    pa.click(542, 382)  # Mouse click at (542, 382)
-    time.sleep(1.30)
-    pa.click(242, 307)  # Mouse click at (242, 307)
-    time.sleep(2.05)
+    time.sleep(2.16)
+    pa.click(81, 137)  # Mouse click at (81, 137)
+    time.sleep(2.16)
+    pa.click(100, 200)
+    time.sleep(4.58)
+    pa.click(147, 298)  # Mouse click at (147, 298)
+    time.sleep(1.44)
     pa.write('s')  # Type: s
-    time.sleep(0.58)
+    time.sleep(0.14)
     pa.write('i')  # Type: i
-    time.sleep(0.50)
-    pa.write('n')  # Type: n
-    time.sleep(0.13)
-    pa.write('n')  # Type: n
-    time.sleep(0.18)
-    pa.write('c')  # Type: c
-    time.sleep(0.79)
-    pa.hotkey('space')  # Hotkey: space
-    time.sleep(0.77)
-    pa.write('s')  # Type: s
-    time.sleep(2.76)
-    pa.click(193, 333)  # Mouse click at (193, 333)
-    time.sleep(1.15)
-    pa.click(135, 427)  # Mouse click at (135, 427)
-    time.sleep(1.62)
-    pa.write('s')  # Type: s
-    time.sleep(0.61)
-    pa.write('i')  # Type: i
-    time.sleep(0.60)
-    pa.write('n')  # Type: n
-    time.sleep(0.19)
-    pa.write('n')  # Type: n
-    time.sleep(0.34)
-    pa.write('c')  # Type: c
-    time.sleep(0.36)
-    pa.hotkey('space')  # Hotkey: space
-    time.sleep(0.15)
-    pa.write('s')  # Type: s
-    time.sleep(1.87)
-    pa.click(136, 485)  # Mouse click at (136, 485)
-    time.sleep(0.89)
-    pa.click(134, 503)  # Mouse click at (134, 503)
-    time.sleep(2.00)
-    pa.click(140, 561)  # Mouse click at (140, 561)
-    time.sleep(1.78)
-    pa.click(129, 584)  # Mouse click at (129, 584)
-    time.sleep(2.10)
-    pa.click(164, 423)  # Mouse click at (164, 423)
-    time.sleep(1.42)
-    pa.click(149, 676)  # Mouse click at (149, 676)
-    time.sleep(1.31)
-    pa.write('g')  # Type: g
-    time.sleep(0.12)
-    pa.write('e')  # Type: e
-    time.sleep(0.12)
-    pa.write('r')  # Type: r
-    time.sleep(2.46)
-    pa.click(151, 652)  # Mouse click at (151, 652)
-    time.sleep(4.06)
-    pa.click(1178, 597)  # Mouse click at (1178, 597)
-    time.sleep(1.05)
-    pa.write('0')  # Type: 0
-    time.sleep(0.15)
-    pa.write('0')  # Type: 0
-    time.sleep(0.10)
-    pa.write('3')  # Type: 3
-    time.sleep(0.12)
-    pa.write('0')  # Type: 0
-    time.sleep(2.76)
-    pa.click(354, 683)  # Mouse click at (354, 683)
-    time.sleep(1.37)
-    pa.click(341, 681)  # Mouse click at (341, 681)
-    time.sleep(2.71)
-    pa.click(164, 337)  # Mouse click at (164, 337)
-    time.sleep(1.87)
-    pa.click(298, 659)  # Mouse click at (298, 659)
-    time.sleep(2.46)
-    pa.click(167, 560)  # Mouse click at (167, 560)
-    time.sleep(3.05)
-    pa.click(604, 350)  # Mouse click at (604, 350)
-    time.sleep(4.41)
-    pa.hotkey('caps lock')  # Hotkey: caps lock
-    time.sleep(0.20)
-    pa.write('B')  # Type: B
-    time.sleep(0.17)
-    pa.hotkey('caps lock')  # Hotkey: caps lock
-    time.sleep(0.18)
-    pa.write('o')  # Type: o
     time.sleep(0.21)
-    pa.write('m')  # Type: m
-    time.sleep(0.15)
-    pa.hotkey('space')  # Hotkey: space
-    time.sleep(0.13)
-    pa.write('d')  # Type: d
-    time.sleep(0.19)
-    pa.write('i')  # Type: i
-    time.sleep(0.07)
-    pa.write('a')  # Type: a
-    time.sleep(0.10)
-    pa.hotkey('space')  # Hotkey: space
-    time.sleep(0.58)
-    pa.hotkey('caps lock')  # Hotkey: caps lock
-    time.sleep(0.24)
-    pa.write('S')  # Type: S
-    time.sleep(0.11)
-    pa.hotkey('caps lock')  # Hotkey: caps lock
-    time.sleep(0.19)
-    pa.write('e')  # Type: e
-    time.sleep(0.21)
-    pa.write('n')  # Type: n
+    pa.write('n')  # Type: nsi
     time.sleep(0.16)
-    pa.write('h')  # Type: h
-    time.sleep(0.05)
-    pa.write('a')  # Type: a
-    time.sleep(0.20)
-    pa.write('s')  # Type: s
-    time.sleep(0.10)
-    pa.hotkey('space')  # Hotkey: space
-    time.sleep(0.83)
-    pa.write('v')  # Type: v
-    time.sleep(0.13)
-    pa.write('e')  # Type: e
-    time.sleep(0.15)
-    pa.write('r')  # Type: r
-    time.sleep(0.23)
-    pa.write('i')  # Type: i
-    time.sleep(0.13)
-    pa.write('f')  # Type: f
-    time.sleep(0.56)
-    pa.write('i')  # Type: i
-    time.sleep(0.41)
+    pa.write('n')  # Type: n
+    time.sleep(0.28)
     pa.write('c')  # Type: c
-    time.sleep(0.22)
-    pa.write('a')  # Type: a
-    time.sleep(0.12)
-    pa.write('d')  # Type: d
+    time.sleep(0.35)
+    pa.hotkey('space')  # Hotkey: space
     time.sleep(0.13)
-    pa.write('a')  # Type: a
-    time.sleep(0.19)
     pa.write('s')  # Type: s
-    time.sleep(1.25)
-    pa.write('.')  # Type: .
+    time.sleep(1.55)
+    pa.click(154, 344)  # Mouse click at (154, 344)
+    time.sleep(1.67)
+    pa.click(122, 420)  # Mouse click at (122, 420)
+    time.sleep(1.48)
+    pa.write('s')  # Type: s
+    time.sleep(0.14)
+    pa.write('i')  # Type: i
+    time.sleep(0.30)
+    pa.write('n')  # Type: n
+    time.sleep(0.18)
+    pa.write('n')  # Type: n
+    time.sleep(0.15)
+    pa.write('c')  # Type: c
+    time.sleep(0.25)
+    pa.hotkey('space')  # Hotkey: space
+    time.sleep(0.26)
+    pa.write('s')  # Type: s
+    time.sleep(1.57)
+    pa.click(124, 486)  # Mouse click at (124, 486)
+    time.sleep(1.00)
+    pa.click(126, 505)  # Mouse click at (126, 505)
+    time.sleep(1.99)
+    pa.click(114, 557)  # Mouse click at (114, 557)
+    time.sleep(2.35)
+    pa.click(127, 693)  # Mouse click at (127, 693)
+    time.sleep(2.39)
+    pa.click(302, 601)  # Mouse click at (302, 601)
+    time.sleep(2.14)
+    pa.click(184, 499)  # Mouse click at (184, 499)
+    time.sleep(1.96)
+    pa.click(168, 594)  # Mouse click at (168, 594)
+    time.sleep(3.28)
+    pa.click(143, 434)  # Mouse click at (143, 434)
+    time.sleep(3.27)
+    pa.click(190, 684)  # Mouse click at (190, 684)
+    time.sleep(2.20)
+    pa.write('g')  # Type: g
+    time.sleep(0.20)
+    pa.write('e')  # Type: e
+    time.sleep(0.09)
+    pa.write('r')  # Type: r
+    time.sleep(1.63)
+    pa.click(189, 645)  # Mouse click at (189, 645)
+    time.sleep(4.04)
+    pa.click(914, 251)  # Mouse click at (914, 251)
+    time.sleep(4.81)
+    pa.write('s')  # Type: s
+    time.sleep(1.02)
+    pa.click(435, 357)  # Mouse click at (435, 357)
+    time.sleep(0.75)
+    pa.write('s')  # Type: s
+    time.sleep(1.60)
+    pa.click(1151, 201)  # Mouse click at (1151, 201)
+    time.sleep(1.82)
+    pa.click(1159, 390)  # Mouse click at (1159, 390)
+    time.sleep(1.35)
+    pa.click(1161, 396)  # Mouse click at (1161, 396)
+    time.sleep(1.09)
+    pa.click(1161, 396)  # Mouse click at (1161, 396)
+    time.sleep(2.28)
+    pa.click(1035, 407)  # Mouse click at (1035, 407)
+    time.sleep(1.13)
+    pa.click(1193, 608)  # Mouse click at (1193, 608)
+    time.sleep(1.32)
+    pa.write('0')  # Type: 0
+    time.sleep(0.15)
+    pa.write('0')  # Type: 0
+    time.sleep(0.82)
+    pa.write('3')  # Type: 3
+    time.sleep(0.17)
+    pa.write('0')  # Type: 0
 
 if __name__ == '__main__':
     play_macro()
